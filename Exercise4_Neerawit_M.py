d66c7307fc3fc3c4175aae1df442fd4b486bcc99
boom20212 <78636366+boom20212@users.noreply.github.com>
2022-04-10 13:49:39 +0700

    Added CP3_Exercise4_Neerawit_M File

---
+++ Exercise4_Neerawit_M.py
@@ -0,0 +1,9 @@
+score_FE = 80 #Foundation English
+score_GB = 67.5 #General Business
+score_ICS = 73 #Introduction to Computer Systems
+score_CP = 75.5 #Computer Programming
+print("--- Your Score ---")
+print("Foundation English:",score_FE)
+print("General Business:",score_GB)
+print("Introduction to Computer Systems:",score_ICS)
+print("Computer Programming:",score_CP)
